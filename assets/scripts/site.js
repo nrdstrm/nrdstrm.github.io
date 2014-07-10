0032bde39fd446a1d270b62bc81425a696bb6283
Robert N <robert.nordstrom@pdb.se>
2014-07-10 21:26:01 +0200

    Parallax

---
+++ assets/scripts/site.js
@@ -0,0 +1,23 @@
+if (window.DeviceOrientationEvent) {
+	alert('DeviceOrientationEvent');
+
+	window.addEventListener('deviceorientation', function (eventData) {
+
+		if (!eventData.beta && !eventData.gamma)
+			return;
+		
+		var y = Math.round((-eventData.beta + 90) * (40/180) - 40);
+		var x = Math.round(-eventData.gamma * (20/180) - 20);
+
+		if (x > 0) {
+			x = -x;
+		} else if (x < -40) {
+			x = -(x + 80);
+		}
+
+		var log = document.getElementById('log');
+		log.value = 'x: ' + x + ', y: ' + y;
+
+	}, false);
+}
+
